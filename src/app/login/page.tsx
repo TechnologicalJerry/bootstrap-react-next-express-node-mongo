e902afb323daeadf8b06dc3eba6ffb2e97bdc028
technologicaljerry <cserajatverma@gmail.com>
2023-10-29 23:32:18 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,2 +1,16 @@
 "use client";
 import Link from "next/link";
+
+export default function LoginPage() {
+    const router = useRouter();
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+
+    })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+
+
+
+}
