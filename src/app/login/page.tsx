fdba856d42af9bb3c22e36a9710c674443b779f2
technologicaljerry <cserajatverma@gmail.com>
2023-10-29 23:34:00 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
-import React, {useEffect} from "react";
-import {useRouter} from "next/navigation";
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 
