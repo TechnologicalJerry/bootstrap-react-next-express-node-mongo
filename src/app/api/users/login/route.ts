238431f5e9501b2c08d12f0c0d88c6c5fe6b18d5
technologicaljerry <cserajatverma@gmail.com>
2023-10-31 07:30:16 +0530

    Update route.ts

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/user.model";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
 
@@ -8,6 +9,12 @@ export async function POST(request: NextRequest) {
         const reqBody = await request.json()
         const { email, password } = reqBody;
         console.log(reqBody);
+
+        const user = await User.findOne({email})
+        if(!user){
+            return NextResponse.json({error: "User does not exist"}, {status: 400})
+        }
+        console.log("user exists");
 
 
 
