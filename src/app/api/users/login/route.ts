6a0c0cfca9ae6292fdfe2cc8e3eb7a5d0f160f0d
technologicaljerry <cserajatverma@gmail.com>
2023-11-01 05:09:40 +0530

    Update route.ts

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -30,6 +30,11 @@ export async function POST(request: NextRequest) {
 
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, { expiresIn: "1d" });
 
+        const response = NextResponse.json({
+            message: "Login successful",
+            success: true,
+        })
+
 
 
     } catch (error: any) {
