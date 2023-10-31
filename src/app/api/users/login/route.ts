15244291ea42431c384cb74009aa47cf2a55dc2f
technologicaljerry <cserajatverma@gmail.com>
2023-11-01 05:08:59 +0530

    Update route.ts

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -22,6 +22,12 @@ export async function POST(request: NextRequest) {
         }
         console.log(user);
 
+        const tokenData = {
+            id: user._id,
+            username: user.username,
+            email: user.email
+        }
+
 
 
     } catch (error: any) {
