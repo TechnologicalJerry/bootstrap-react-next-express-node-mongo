0d6a542e71fa5fe7e1362ea5fbb3aab8ba92d6f3
technologicaljerry <cserajatverma@gmail.com>
2023-10-31 07:26:14 +0530

    Update route.ts

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,3 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+
+export async function POST(request: NextRequest){
+    try {
+
+        const reqBody = await request.json()
+        const {email, password} = reqBody;
+        console.log(reqBody);
+
+        
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
