cdd8d690a4abff04975269dcc1fa97551aa3ed4c
technologicaljerry <cserajatverma@gmail.com>
2023-11-02 23:53:27 +0530

    Update page.tsx

--- src/app/api/users/signup/page.tsx
+++ src/app/api/users/signup/page.tsx
@@ -3,3 +3,5 @@ import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
 import { sendEmail } from "@/helpers/mailer";
+
+connect();
