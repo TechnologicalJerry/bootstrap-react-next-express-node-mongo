8e952db10d3b9e559c254caff08e0940dba45955
technologicaljerry <cserajatverma@gmail.com>
2023-11-03 23:04:15 +0530

    Update route.ts

--- src/app/api/users/user/route.ts
+++ src/app/api/users/user/route.ts
@@ -5,17 +5,17 @@ import User from "@/models/userModel";
 
 connect();
 
-export async function GET(request:NextRequest){
+export async function GET(request: NextRequest) {
 
     try {
         const userId = await getDataFromToken(request);
-        const user = await User.findOne({_id: userId}).select("-password");
+        const user = await User.findOne({ _id: userId }).select("-password");
         return NextResponse.json({
             mesaaage: "User found",
             data: user
         })
-    } catch (error:any) {
-        return NextResponse.json({error: error.message}, {status: 400});
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 400 });
     }
 
 }
