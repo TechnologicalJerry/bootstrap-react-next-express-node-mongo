8ee93a470ce82da510bbd4a8767b83dc074a29db
technologicaljerry <cserajatverma@gmail.com>
2024-03-14 22:17:15 +0530

    Update default.ts

--- express-server/config/default.ts
+++ express-server/config/default.ts
@@ -1,4 +1,4 @@
 export default {
   port: 5050,
-  databadeUrl: "mongodb://localhost:27017/bootstrap-angular-express-node-mongo",
+  databadeUrl: "mongodb://localhost:27017/bootstrap-react-next-express-node-mongo",
 };
